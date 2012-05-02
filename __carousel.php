a3847ca7addb45ac353513124d2cd8a4aefa7abc
Russ Hall <russ@2tap.com>
2012-05-02 10:01:29 +0100

    Hide carousel arrows when only one image

--- __carousel.php
+++ __carousel.php
@@ -5,8 +5,10 @@
     if($data) {
 ?>
 
-<div class="showcase_carousel">
+<div class="showcase_carousel image-num-<?php echo $data['image_num'] ?>">
+
     <a href="#" class="arrow action left"></a>
+
     <ol>
     <?php
         for($i = 1; $i < $data['image_num'] + 1; $i++) {
@@ -19,7 +21,9 @@
         }
     ?>
     </ol>
+    
     <a href="#" class="arrow action right"></a>
+    
     <?php if($data['image_num'] > 1) { ?>
     <div class="pip_container">
         <?php for($i = 0; $i < $data['image_num']; $i++) { ?>
