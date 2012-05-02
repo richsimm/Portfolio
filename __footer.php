e17abdf033a04638423208a5cb9e0d94cf938b87
Russ Hall <russ@2tap.com>
2012-05-02 09:18:54 +0100

    Refactored image switching. Implemented pip switching

--- __footer.php
+++ __footer.php
@@ -1,25 +1,36 @@
     <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>-->
     <script src="js/jquery-1.7.1.min.js"></script>
     <script>
-        $(document).ready(function() {
+        (function() {
 
-            function changeToImage(index) {
+            function changeToImage(carousel, index) {
 
+                // Grab list elements
+                var list_items = carousel.find('ol li');
 
+                // Switch current and next image active classes
+                list_items.removeClass('active');
+                $(list_items.get(index)).addClass('active');
+
+                // Update pips
+                var pips = carousel.find('.pip_container a');
+                pips.removeClass('active');
+                $(pips.get(index)).addClass('active');
             }
 
             $(document).on('click', '.showcase_carousel .action', function(event) {
 
-                var arrow = $(event.target);
-
-                // Kill default
                 event.preventDefault();
 
-                // Grab list element
-                var list = arrow.parents('.showcase_carousel').find('ol');
+                // Retrieve which arrow was clicked
+                var arrow = $(event.target);
+
+                // Grab carousel parent element
+                var carousel = arrow.parents('.showcase_carousel');
+                var list = carousel.find('ol');
 
                 // Grab current active item
-                var active = list.find('.active');
+                var active = list.find('li.active');
 
                 // Determine next list element to show
                 if(arrow.hasClass('left')) {
@@ -37,60 +48,67 @@
                     }
                 }
 
-                // Update pips
-                var pips = arrow.parents('.showcase_carousel').find('.pip_container a');
-                var imageIndex = list.find('li').index(next);
-
-                pips.removeClass('active');
-                $(pips.get(imageIndex)).addClass('active');
-
-                // Switch active states on current and next list element
-                active.removeClass('active');
-                next.addClass('active');
+                changeToImage(carousel, next.index());
             });
-        });
 
-        $(document).on('click', '.work_menu_square', function(event) {
+            $(document).on('click', '.pip_container a', function() {
 
-            var anchor = $(event.target);
-            event.preventDefault();
+                event.preventDefault();
 
-            if(!anchor.attr('data-key')) {
-                anchor = anchor.parents('a[data-key]');
-            }
+                var pip = $(event.target);
 
-            var project = anchor.attr('data-key');
+                if(!pip.hasClass('pip')) {
+                    pip = pip.parents('a.pip');
+                }
+                
+                var carousel = pip.parents('.showcase_carousel');
 
-            // Switch active states
-            $('.work_menu_square').removeClass('active');
-            anchor.addClass('active');
-
-            // Empty current carousel and description
-            var parent = anchor.parents('#container');
-            var carousel = parent.find('.showcase_carousel');
-            var description = parent.find('.panel_inner');
-            carousel.html('');
-            description.html('');
-
-            // Fetch data
-            $.ajax({
-                url: 'ajax',
-                data: {
-                    'project': project
-                },
-                success: function(html) {
-
-                    var result = $(html);
-
-                    // Update carousel
-                    carousel.html(result.find('.showcase_carousel'));
-
-                    // Update description
-                    description.html(result.find('.content_info'));
-                },
-                dataType: 'html'
+                changeToImage(carousel, pip.index());
             });
-        });
+
+            $(document).on('click', '.work_menu_square', function(event) {
+
+                event.preventDefault();
+
+                var anchor = $(event.target);
+                
+                if(!anchor.attr('data-key')) {
+                    anchor = anchor.parents('a[data-key]');
+                }
+
+                var project = anchor.attr('data-key');
+
+                // Switch active states
+                $('.work_menu_square').removeClass('active');
+                anchor.addClass('active');
+
+                // Empty current carousel and description
+                var parent = anchor.parents('#container');
+                var carousel = parent.find('.showcase_carousel');
+                var description = parent.find('.panel_inner');
+                carousel.html('');
+                description.html('');
+
+                // Fetch data
+                $.ajax({
+                    url: 'ajax',
+                    data: {
+                        'project': project
+                    },
+                    success: function(html) {
+
+                        var result = $(html);
+
+                        // Update carousel
+                        carousel.html(result.find('.showcase_carousel'));
+
+                        // Update description
+                        description.html(result.find('.content_info'));
+                    },
+                    dataType: 'html'
+                });
+            });
+        })();
 
     </script>
     </body>
